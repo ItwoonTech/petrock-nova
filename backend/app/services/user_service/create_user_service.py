31e9ccb4f8e2865589e4a791f82675fcc393ba95
shunsei <k.shunsei1227@gmail.com>
2025-07-06 15:57:26 +0900

    generate pet id when create user

--- backend/app/services/user_service/create_user_service.py
+++ backend/app/services/user_service/create_user_service.py
@@ -1,3 +1,5 @@
+import uuid
+
 from pydantic import BaseModel
 
 from app.models.user import User, UserRole
@@ -6,7 +8,6 @@ from app.repositories.interface.user_repository import UserRepository
 
 class CreateUserServiceRequest(BaseModel):
     user_id: str
-    pet_id: str
     user_name: str
     user_role: str
     password: str
@@ -21,14 +22,14 @@ class CreateUserService:
         self.user_repository = user_repository
 
     def execute(self, request: CreateUserServiceRequest) -> CreateUserServiceResponse:
-        user = User(
+        new_user = User(
             user_id=request.user_id,
-            pet_id=request.pet_id,
+            pet_id=str(uuid.uuid4()),
             user_name=request.user_name,
             user_role=UserRole(request.user_role),
             password=request.password,
         )
 
-        created_user = self.user_repository.create(user)
+        created_user = self.user_repository.create(new_user)
 
         return CreateUserServiceResponse(user=created_user)
