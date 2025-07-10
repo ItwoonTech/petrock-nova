fc9c4185bd90356a4696d89aa4426fa315b223df
shunsei <k.shunsei1227@gmail.com>
2025-07-10 23:29:48 +0900

    wrap fastapi app by mangum

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+# flake8: noqa: E402
 # fmt: off
 from dotenv import load_dotenv
 
@@ -5,9 +6,12 @@ load_dotenv()
 # fmt: on
 
 from fastapi import FastAPI
+from mangum import Mangum
 
 from app.api.routers.user_router import router as user_router
 
 app = FastAPI()
 
 app.include_router(user_router, prefix="/users", tags=["User"])
+
+handler = Mangum(app)
