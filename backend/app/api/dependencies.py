ce79a96c8f56c5603b50b102649a456bde263cd2
shunsei <k.shunsei1227@gmail.com>
2025-07-13 15:09:46 +0900

    add create user dependency

--- backend/app/api/dependencies.py
+++ backend/app/api/dependencies.py
@@ -4,6 +4,7 @@ from fastapi import Depends
 
 from app.repositories.dynamodb.user_repository import DynamoDBUserRepository
 from app.repositories.interface.user_repository import UserRepository
+from app.services.user_service.create_user_service import CreateUserService
 from app.services.user_service.get_user_service import GetUserService
 
 USER_TABLE_NAME = os.getenv("USER_TABLE_NAME")
@@ -17,3 +18,9 @@ def get_get_user_service(
     user_repository: UserRepository = Depends(get_user_repository),
 ) -> GetUserService:
     return GetUserService(user_repository)
+
+
+def get_create_user_service(
+    user_repository: UserRepository = Depends(get_user_repository),
+) -> CreateUserService:
+    return CreateUserService(user_repository)
