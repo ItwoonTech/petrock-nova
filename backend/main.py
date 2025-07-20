502e1fe4187c422721ec1bfc86375b7917a9e800
shunsei <k.shunsei1227@gmail.com>
2025-07-20 10:50:19 +0900

    add s3 router

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 from mangum import Mangum
 
 from app.api.routers.pet_router import router as pet_router
+from app.api.routers.s3_router import router as s3_router
 from app.api.routers.user_router import router as user_router
 
 app = FastAPI()
 
 app.include_router(user_router, prefix="/users", tags=["User"])
 app.include_router(pet_router, prefix="/pets", tags=["Pet"])
+app.include_router(s3_router, prefix="/s3", tags=["S3"])
 
 handler = Mangum(app)
