0f2bcca387d760d8e6b63655ce525dac2d8ed718
shunsei <k.shunsei1227@gmail.com>
2025-07-26 03:32:23 +0900

    refactor image repository

--- backend/app/repositories/s3/image_repository.py
+++ backend/app/repositories/s3/image_repository.py
@@ -27,17 +27,17 @@ class S3ImageRepository(ImageRepository):
 
         self.bucket_name = bucket_name
 
-    def get_image(self, image_path: str) -> bytes | None:
+    def get_image(self, image_key: str) -> bytes | None:
         """
         S3から画像を取得する
 
         Args:
-            image_path (str): 画像のパス
+            image_key (str): 画像のキー
 
         Returns:
             bytes | None: 画像のバイナリデータ（存在しない場合はNone）
         """
-        response = self.bucket.get_object(Bucket=self.bucket_name, Key=image_path)
+        response = self.bucket.get_object(Bucket=self.bucket_name, Key=image_key)
 
         if "Body" not in response:
             return None
@@ -46,26 +46,26 @@ class S3ImageRepository(ImageRepository):
 
     def put_image(
         self,
-        image_path: str,
+        image_key: str,
         image_bytes: bytes,
     ) -> None:
         """
         S3に画像を保存する
 
         Args:
-            image_path (str): 保存する画像のパス
+            image_key (str): 保存する画像のキー
             image_bytes (bytes): 保存する画像のバイナリデータ
         """
         self.bucket.put_object(
             Bucket=self.bucket_name,
-            Key=image_path,
+            Key=image_key,
             Body=image_bytes,
         )
 
     def get_presigned_url(
         self,
         client_method: str,
-        file_name: str,
+        image_key: str,
         expires_in: int = 3600,
         http_method: str = None,
     ) -> str:
@@ -74,7 +74,7 @@ class S3ImageRepository(ImageRepository):
 
         Args:
             client_method (str): 署名付きURLによって許可する操作
-            file_name (str): アクセスするファイル名
+            image_key (str): アクセスする画像のキー
             expires_in (int, optional): URLの有効期限 (秒単位)
             http_method (str, optional): 署名付きURLによって許可するHTTPメソッド
 
@@ -85,7 +85,7 @@ class S3ImageRepository(ImageRepository):
             client_method,
             Params={
                 "Bucket": self.bucket_name,
-                "Key": file_name,
+                "Key": image_key,
             },
             ExpiresIn=expires_in,
             HttpMethod=http_method,
