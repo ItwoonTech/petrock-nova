c48919a75adf9437dbfc818048523d7c79baac70
shunsei <k.shunsei1227@gmail.com>
2025-07-28 11:00:24 +0900

    add docstirng to pet picture desc client interface

--- backend/app/ai/interface/pet_picture_description_client.py
+++ backend/app/ai/interface/pet_picture_description_client.py
@@ -4,4 +4,13 @@ from abc import ABC, abstractmethod
 class PetPictureDescriptionClient(ABC):
     @abstractmethod
     def describe(self, s3_image_key: str) -> str:
+        """
+        ペットの画像から説明文を生成する
+
+        Args:
+            s3_image_key (str): ペットの画像のS3キー
+
+        Returns:
+            str: ペットの画像の説明文
+        """
         pass
