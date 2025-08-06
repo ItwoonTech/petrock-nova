91dda5fd850355c38f4134ae3ea653d67202470a
rice0416 <183781402+rice0416@users.noreply.github.com>
2025-08-06 18:29:45 +0900

    add: update_pet_service

---
+++ backend/app/services/pet_service/update_pet_service.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+from app.models.pet import Pet, PetCareNote, PetGender
+from app.repositories.interface.pet_repository import PetRepository
+
+
+class UpdatePetServiceRequest(BaseModel):
+    pet_id: str
+    care_notes: list[PetCareNote]
+
+
+class UpdatePetServiceResponse(BaseModel):
+    pet_id: str
+    name: str
+    category: str
+    birth_date: datetime
+    gender: PetGender
+    care_notes: list[PetCareNote]
+    image_name: str
+    created_at: datetime
+    updated_at: datetime
+
+
+class UpdatePetService:
+    def __init__(self, pet_repository: PetRepository):
+        self.pet_repository = pet_repository
+
+    def execute(self, request: UpdatePetServiceRequest) -> UpdatePetServiceResponse:
+        current_pet = self.pet_repository.get_by_id(request.pet_id)
+
+        if current_pet is None:
+            raise ValueError("ユーザーが見つかりませんでした")
+
+        updated_pet: Pet = current_pet.update(**request.model_dump())
+        self.pet_repository.update(updated_pet)
+
+        return UpdatePetServiceResponse(**updated_pet.to_dict())
