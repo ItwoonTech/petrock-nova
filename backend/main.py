f90d1ffc08bc5ffa8643e3830ece6f87411ef2b8
kai-shellfish <kai1220tennis@gmail.com>
2025-08-07 23:14:15 +0900

    main.pyにdiaryのprefixを追加

--- backend/main.py
+++ backend/main.py
@@ -4,11 +4,13 @@ from mangum import Mangum
 from app.api.routers.pet_router import router as pet_router
 from app.api.routers.s3_router import router as s3_router
 from app.api.routers.user_router import router as user_router
+from app.api.routers.diary_router import router as diary_router
 
 app = FastAPI()
 
 app.include_router(user_router, prefix="/users", tags=["User"])
 app.include_router(pet_router, prefix="/pets", tags=["Pet"])
 app.include_router(s3_router, prefix="/s3", tags=["S3"])
+app.include_router(diary_router, prefix="/diarys", tags=["Diary"])
 
 handler = Mangum(app)
