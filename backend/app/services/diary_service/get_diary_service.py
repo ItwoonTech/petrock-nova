8c74abc07654f42e231b6648f8bedc3bc8bd801f
shunsei <k.shunsei1227@gmail.com>
2025-08-10 14:21:21 +0900

    update service

--- backend/app/services/diary_service/get_diary_service.py
+++ backend/app/services/diary_service/get_diary_service.py
@@ -2,24 +2,23 @@ from datetime import date, datetime
 
 from pydantic import BaseModel
 
-from app.models.diary import DiaryTask
-from app.models.types import ContentfulString
+from app.models.diary import DiaryTask, Weather
 from app.repositories.interface.diary_repository import DiaryRepository
 
 
 class GetDiaryServiceRequest(BaseModel):
-    pet_id: ContentfulString
+    pet_id: str
     date: date
 
 
 class GetDiaryServiceResponse(BaseModel):
-    pet_id: ContentfulString
-    date: ContentfulString
-    picture_name: ContentfulString
+    pet_id: str
+    date: date
+    picture_name: str
     reacted: bool
-    advice: ContentfulString
-    comment: ContentfulString
-    weather: ContentfulString
+    advice: str
+    comment: str
+    weather: Weather
     temperature: float
     task: list[DiaryTask]
     created_at: datetime
